f31be33f8ca2d6d9f71563bef0a2aae6976ce0b9
BlueRasputin <barrettjoshrev10@gmail.com>
2025-03-16 13:51:58 -0500

    completed password authenticator

--- password-validator.js
+++ password-validator.js
@@ -3,35 +3,29 @@ let passwordIsValid = false;
 let password;
 
 while (!passwordIsValid) {
-
-    //Start with variables can store the booleans tied to password requirements
+    
     let passLongEnough = false;
     let upperCase = false;
     let hasNum = false;
-
-    //Prompt User for Password Entry
+    
     password = readlineSync.question("Please enter a valid Password: ")
 
-
-    //Check for at least 8 characters in password
+    
     if (password.length < 8) {
         console.log("Password requires at least 8 characters.");
         } else {
             passLongEnough = true;
         }
 
-    //Loop through each character in the password
-
     for (let i = 0; i < password.length; i++) {
         let charCode = password.charCodeAt(i);
+    
         
-
-        //Check for upper case letters (which is Unicode 65 to 90)
         if (charCode >= 65 && charCode <= 90) {
             upperCase = true;
         }
 
-        //Check for a number in the password (Unicode 48 to 57)
+        
         if (charCode >= 48 && charCode <= 57) {
             hasNum = true;
         }
@@ -46,7 +40,7 @@ while (!passwordIsValid) {
     }
 
 
-    //Final check for all conditions met
+    
     if (passLongEnough && upperCase && hasNum) {
         passwordIsValid = true;
     }
